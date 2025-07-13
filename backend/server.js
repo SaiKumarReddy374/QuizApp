198559fa826f3a13ee908da3799b81861007466d
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-07-13 20:09:03 +0530

    project

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from "path";
+import { fileURLToPath } from 'url';
 
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js'; // Ensure the extension is `.js`
@@ -33,12 +34,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/student', studentRouter); 
 app.use('/api/teacher',teacherRouter);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
-
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-  })
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
 }
 
 // MongoDB Connection
