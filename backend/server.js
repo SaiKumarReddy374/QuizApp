dc7869ca2467b76985a75814312a4b2e35626ab8
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-07-14 11:34:31 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import path from 'path';
 
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js'; // Ensure the extension is `.js`
@@ -14,22 +14,35 @@ import { askAI } from './controllers/aiController.js';
 
 dotenv.config();
 
+const __dirname=path.resolve();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
-app.use(express.json());
-app.use(cookieParser());
 
 app.post('/api/student/ask-ai', askAI);
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/student', studentRouter); 
 app.use('/api/teacher',teacherRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  const frontendPath = path.join(__dirname, '../../frontend/dist');
+  app.use(express.static(frontendPath));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+  });
+}
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
