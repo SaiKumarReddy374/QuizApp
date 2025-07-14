2f701292a75f34f32178916ad8a510d18463f97a
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-07-14 13:16:41 +0530

    server fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,11 +27,11 @@ export const login = async (req, res) => {
     );
 
     res.cookie('token', token, {
-      httpOnly: true,
-      secure:process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+  httpOnly: true,
+  secure: true,         // ✅ required with sameSite: 'None'
+  sameSite: 'None',     // ✅ allows cross-site cookie use
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
     console.log(`User logged in: ${email}`);
     res.json({
